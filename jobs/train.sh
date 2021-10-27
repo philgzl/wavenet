51a57a0a6a6d6055aeb02a08b4a1939d0b2f3df4
philgzl <services@philgzl.com>
2021-10-27 13:24:29 +0200

    Update jobs and convert from DOS to UNIX

--- jobs/train.sh
+++ jobs/train.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
-### General options
-### -- specify queue --
-#BSUB -q gpuv100
-### -- set the job Name --
-#BSUB -J jobname
-### -- ask for number of cores (default: 1) --
-#BSUB -n 8
-### -- Select the resources: 1 gpu in exclusive process mode --
-#BSUB -gpu "num=1:mode=exclusive_process"
-### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues
-#BSUB -W 24:00
-### -- request amount of system-memory --
-#BSUB -R "rusage[mem=4GB]"
-### -- Specify the output and error file. %J is the job-id --
-### -- -o and -e mean append, -oo and -eo mean overwrite --
-#BSUB -oo jobs/logs/%J.out
-#BSUB -eo jobs/logs/%J.err
-### -- Specify the distribution of the cores: on a single node --
-#BSUB -R "span[hosts=1]"
-# -- end of LSF options --
 
-source venv/bin/activate
-python scripts/train.py
+OPTS=`getopt -o f -l force -- "$@"`
+if [ $? -ne 0 ]; then exit 1; fi
+eval set -- "$OPTS"
+
+
+FORCE=false
+while true
+do
+  case "$1" in
+    -f | --force ) FORCE=true; shift ;;
+    -- ) shift; break ;;
+  esac
+done
+
+if [ "$FORCE" = true ]
+then
+    COMMAND="python scripts/train.py -f"
+else
+    COMMAND="python scripts/train.py"
+fi
+
+mkdir -p jobs/logs
+bash jobs/submit.sh jobs/job.sh "$COMMAND"
