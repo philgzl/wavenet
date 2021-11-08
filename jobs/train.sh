765e685151f1ad1a9251843cb413e31e01fbe1a9
Philippe <philgzl@outlook.com>
2021-11-08 11:38:27 +0100

    Make mixed precision an option in job script too

--- jobs/train.sh
+++ jobs/train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 OPTS=$(getopt \
-    --longoptions ignore-checkpoint,workers: \
+    --longoptions ignore-checkpoint,workers,mixed-precision: \
     --options "" \
     -- "$@"
 )
@@ -10,11 +10,13 @@ eval set -- "$OPTS"
 
 IGNORE_CHECKPOINT=false
 WORKERS=8
+MIXED_PRECISION=true
 while true
 do
   case "$1" in
     --ignore-checkpoint ) IGNORE_CHECKPOINT=true; shift ;;
     --workers ) WORKERS="$2"; shift; shift ;;
+    --mixed-precision ) MIXED_PRECISION=true; shift ;;
     -- ) shift; break ;;
   esac
 done
@@ -27,6 +29,10 @@ do
   if [ "$IGNORE_CHECKPOINT" = true ]
   then
     COMMAND="${COMMAND} --ignore-checkpoint"
+  fi
+  if [ "$MIXED_PRECISION" = true ]
+  then
+    COMMAND="${COMMAND} --mixed-precision"
   fi
   bash jobs/submit.sh jobs/job.sh "$COMMAND"
 done
