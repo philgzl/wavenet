7f3bc4a5cb0d6c598f5ede96d74e00b53d63e2b2
Philippe Gonzalez <philgzl@outlook.com>
2021-11-22 09:17:17 +0100

    Add generation job script

---
+++ jobs/generate.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+OPTS=$(getopt \
+    --longoptions n-samples:, \
+    --options n: \
+    -- "$@"
+)
+if [ $? -ne 0 ]; then exit 1; fi
+eval set -- "$OPTS"
+
+N_SAMPLES=160000
+while true
+do
+  case "$1" in
+    -n | --n-samples ) N_SAMPLES="$2"; shift; shift ;;
+    -- ) shift; break ;;
+  esac
+done
+
+mkdir -p jobs/logs
+
+for INPUT in "$@"
+do
+  COMMAND="python scripts/generate.py ${INPUT} -n ${WORKERS}"
+  bash jobs/submit.sh jobs/job.sh "$COMMAND"
+done
